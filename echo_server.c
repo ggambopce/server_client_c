b2d500415c6e395d92eb916a3e4e9cbde03ebeec
ggambopce <ggambopce@gmail.com>
2025-07-22 16:50:01 +0900

    UDP 클라이언트 작성

--- echo_server.c
+++ echo_server.c
@@ -13,16 +13,26 @@
 #define BUF_SIZE 1024               // 일반적으로 통신에 가장많이 쓰이는 버퍼크기 read(), recv() 부담없이 처리
 void error_handling(char *message);
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])    // 넘어오는 인자의 갯수와 문자열 포인터들의 배열의 포인터
 {
-    int serv_sock;
-    int clnt_sock;
+    int serv_sock;                  // 연결 대기 전용 서버 소켓
+    int clnt_sock;                  // 클라이언트 소켓
 
-    char message[BUF_SIZE];
-    int str_len, i;
+    char message[BUF_SIZE];         // 데이터를 주고받을 버퍼    
+    int str_len, i;                 // 문자열 길이와 반복문용 상수
 
-    struct sockaddr_in serv_addr, clnt_adr;
-    socklen_t clnt_adr_sz;
+    /* 대략 이렇게 생김
+        struct sockaddr_in {              // IPv4용 주소정보를 저장하기 위한 구조체 
+            sa_family_t     sin_family;   // 주소 체계 (IPv4이면 AF_INET)
+            in_port_t       sin_port;     // 포트 번호 (2바이트, 네트워크 바이트 순서)
+            struct in_addr  sin_addr;     // IP 주소 (4바이트)
+            char            sin_zero[8];  // 사용 안 함 (패딩용)
+        };
+
+     */
+    struct sockaddr_in serv_addr;    // 서버자신의 IP/포트 정보를 저장 bind() 사용                
+    struct sockaddr_in clnt_adr;     // 접속해 온 클라이언트의 IP/포트 정보를 저장 accept 사용 
+    socklen_t clnt_adr_sz;           // 클라이언트의 주소 크기 accept()에서 사용
 
     if(argc != 2)
 	{
@@ -66,7 +76,21 @@ int main(int argc, char *argv[])
 
 void error_handling(char *message)
 {
-    fputs(message, stderr);
-	fputc('\n', stderr);
+    fputs(message, stderr);             // 출력할 문자열과 출력 대상 파일 스트림
+	fputc('\n', stderr);                // 줄바꿈 문자 하나 출력
 	exit(1);
 }
+
+/*
+int fputs(const char *str, FILE *stream)로 보는 char 사용
+| 표현           | 의미                           |
+| ------------- | ---------------------------- |
+| `char`        | 문자 하나                        |
+| `char[]`      | 문자들의 배열 (문자열)                |
+| `char*`       | 문자 배열의 시작 주소 (문자열을 가리키는 포인터) |
+| `const char*` | 읽기 전용 문자열 포인터                |
+
+* 포인터 만으로는 해당 자료의 크기를 알수 없다.
+* sizeof도 포인터의 크기만 반환
+* 포인터가 가리키는 자료형의 길이를 함께 넣어줘야 한다.
+ */
