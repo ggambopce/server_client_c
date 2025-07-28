5823d48cb7f127cb45c221247995a365570b97fe
ggambopce <ggambopce@gmail.com>
2025-07-28 14:45:07 +0900

    멀티스레드 서버 구현

--- tcp_server/tcp_server.c
+++ tcp_server/tcp_server.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h> // socket, bind, listen, accept
 #include <arpa/inet.h>  // sockaddr_in, htons, inet_ntoa
 #include "mysql.h"
+#include <pthread.h>
 
 #define BUF_SIZE 1024 // 수신 버퍼 크기
 void error_handling(char *message);
@@ -16,21 +17,85 @@ extern int writeDB(MYSQL * mysql, float temp, float hum, int device_id);
 extern int readDB(MYSQL*, char * buff, int size, int idRow);
 extern int closeDB(MYSQL *);
 
+// 멀티스레드 구조체 선언 
+struct ThreadArgs {
+    int client_socket; // 연결된 클라이언트 소켓 정보를 담을 변수
+    struct sockaddr_in client_address;
+};
+
+// 
+void* handle_client(void* arg) {
+    struct ThreadArgs *args = (struct ThreadArgs*)arg;
+    int client_socket = args->client_socket;
+    struct sockaddr_in client_address = args->client_address;
+    char message[BUF_SIZE]; // 버퍼만큼 수신 데이터를 저장할 변수
+    int string_length;      // read, rece등 반환값인 문자열 길이를 받을 변수
+
+    printf("클라이언트 연결 성공 %s", inet_ntoa(client_address.sin_addr));
+
+    MYSQL mysql;
+    if (initDB(&mysql, "localhost", "root", "1234", "plantdb") < 0) 
+    {
+        fprintf(stderr, "(!) DB 연결 실패");
+        close(client_socket);
+        free(arg);
+        pthread_exit(NULL);
+    }
+
+        // 6. 데이터 통신 서비스 로직
+    while ((string_length = read(client_socket, message, BUF_SIZE)) > 0)
+    {
+        message[string_length] = '\0';                  // 수신된 문자열에 끝에 널 종료 문자 삽입
+        printf("수신된 센서 원본 데이터: %s", message); // 원본메세지 콘솔에 출력
+
+        // JSON 구조화를 위한 변수 선언
+        double temp = 0.0, hum = 0.0;
+        int device_id = -1; // 기본값 -1로 설정
+
+
+        char *temp_ptr = strstr(message, "TEMP="); // TEMP= 키워드가 처음 등장하는 위치의 포인터
+        char *hum_ptr = strstr(message, "HUM=");   // HUM= 키워드가 처음 등장하는 위치의 포인터
+        char *id_ptr = strstr(message, "ID=");
+
+        // TEMP, HUM 모두 존재할 경우에만 파싱 시도
+        if (temp_ptr && hum_ptr && id_ptr)
+        {
+            // 함수원형 int sscanf(const char *str, const char *format, ...);
+            // (분석할 문자열 주소값, 추출할 형식 지정문자열, 실제데이터 저장할 변수의 포인터)
+            sscanf(temp_ptr, "TEMP=%lf", &temp); // TEMP 값 추출
+            sscanf(hum_ptr, "HUM=%lf", &hum);    // HUM 값 추출
+            sscanf(id_ptr,   "ID=%d",  &device_id);
+
+            // 추출한 값을 JSON 형식으로 출력
+            printf("파싱된 JSON: { \"temperature\": %.2f, \"humidity\": %.2f, \"device_id\": %d }\n", temp, hum, device_id);
+
+            // DB에 저장
+            writeDB(&mysql, temp, hum, device_id);
+
+        }
+        else
+        {
+            // TEMP 또는 HUM 키가 누락된 경우 경고 출력
+            printf("TEMP 또는 HUM 포맷 오류\n");
+        }
+
+        // 클라이언트에게 에코 응답보내기
+        write(client_socket, message, string_length);
+    }
+
+    // 7. 자원 해제
+    close(client_socket);
+    closeDB(&mysql);
+    free(arg);
+    pthread_exit(NULL);
+}
+
+
 // 아두이노 - 라즈베리파이 간 TCP서버
 int main(int argc, char *argv[])
 {
-    // DB 연결
-    MYSQL mysql;
-    if (initDB(&mysql, "localhost", "root", "1234", "plantdb") < 0) {
-        printf("(!) DB 연결 실패\n");
-        exit(1);
-    }
 
     int server_socket; // 연결 대기 전용 서버 소켓 식별자
-    int client_socket; // 연결된 클라이언트 소켓 정보를 담을 변수
-
-    char message[BUF_SIZE]; // 버퍼만큼 수신 데이터를 저장할 변수
-    int string_length;      // read, rece등 반환값인 문자열 길이를 받을 변수
 
     struct sockaddr_in server_address; // 서버자신의 IP/포트 정보를 저장, bind()에서 사용
     struct sockaddr_in client_address; // 접속해 온 클라이언트의 IP/포트 정보를 저장 accept 사용
@@ -68,57 +133,30 @@ int main(int argc, char *argv[])
     else
         printf("서버가 시작되었습니다. 포트 %d에서 대기 중...\n", atoi(argv[1]));
 
-    // 5.클라이언트 접속 수락
-    client_address_size = sizeof(client_address); // 클라이언트 구조체 길이 받아올 준비
+    while(1) {
+        // 5.클라이언트 접속 수락
+        client_address_size = sizeof(client_address); // 클라이언트 구조체 길이 받아올 준비
+        // 함수 원형 int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+        // (listen()중인 서버소캣파일 디스크립터, 연결요청 온 클라이언트 주소정보를 담고있는 구조체 포인터, 주소 구조체 크기 포인터)
+        // 블로킹 호출, 연결요청이 수락되면 통신 전용 소캣 디스크립터 반환 - 통신 소캣 생성(client_socket)
+        int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_size);
 
-    // 함수 원형 int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
-    // (listen()중인 서버소캣파일 디스크립터, 연결요청 온 클라이언트 주소정보를 담고있는 구조체 포인터, 주소 구조체 크기 포인터)
-    // 블로킹 호출, 연결요청이 수락되면 통신 전용 소캣 디스크립터 반환 - 통신 소캣 생성(client_socket)
-    client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_size);
-    if (client_socket == -1)
-        error_handling("클라이언트 연결 수락 실패");
-    else
-        printf("클라이언트 연결 성공 %s \n", inet_ntoa(client_address.sin_addr));
-
-    // 6. 데이터 통신 서비스 로직
-    while ((string_length = read(client_socket, message, BUF_SIZE)) != 0)
-    {
-        message[string_length] = '\0';                  // 수신된 문자열에 끝에 널 종료 문자 삽입
-        printf("수신된 센서 원본 데이터: %s", message); // 원본메세지 콘솔에 출력
-
-        // JSON 구조화를 위한 변수 선언
-        double temp = 0.0, hum = 0.0;
-        char *temp_ptr = strstr(message, "TEMP="); // TEMP= 키워드가 처음 등장하는 위치의 포인터
-        char *hum_ptr = strstr(message, "HUM=");   // HUM= 키워드가 처음 등장하는 위치의 포인터
-
-        // TEMP, HUM 모두 존재할 경우에만 파싱 시도
-        if (temp_ptr && hum_ptr)
+        if (client_socket == -1)
         {
-            // 함수원형 int sscanf(const char *str, const char *format, ...);
-            // (분석할 문자열 주소값, 추출할 형식 지정문자열, 실제데이터 저장할 변수의 포인터)
-            sscanf(temp_ptr, "TEMP=%lf", &temp); // TEMP 값 추출
-            sscanf(hum_ptr, "HUM=%lf", &hum);    // HUM 값 추출
-
-            // 추출한 값을 JSON 형식으로 출력
-            printf("파싱된 JSON: { \"temperature\": %.2f, \"humidity\": %.2f }\n", temp, hum);
-
-            // DB에 저장 (device_id는 예시로 1번 사용)
-            int device_id = 1;
-            writeDB(&mysql, temp, hum, device_id);
-
+            error_handling("클라이언트 연결 수락 실패");
+            continue;
         }
-        else
-        {
-            // TEMP 또는 HUM 키가 누락된 경우 경고 출력
-            printf("TEMP 또는 HUM 포맷 오류\n");
-        }
+       
+        struct ThreadArgs *args = malloc(sizeof(struct ThreadArgs));
+        args->client_socket = client_socket;
+        args->client_address = client_address;
 
-        // 클라이언트에게 에코 응답보내기
-        write(client_socket, message, string_length);
-    }
+        pthread_t t_id;
+        pthread_create(&t_id, NULL, handle_client, args);
+        pthread_detach(t_id);
+    }    
 
-    // 7. 자원 해제
-    close(client_socket);
+
     close(server_socket);
 
     return 0;
